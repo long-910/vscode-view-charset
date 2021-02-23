7d42a1ab9b3bcd3a6115b1b13b3ee7912ef4f2f5
long-kudo <910.long@gmail.com>
2021-02-23 21:01:53 +0900

    Resolved error

--- src/extension.ts
+++ src/extension.ts
@@ -6,9 +6,11 @@ export function activate(context: vscode.ExtensionContext) {
 
 	console.log('extension "vscode-view-charset" is now active!');
 
-	vscode.window.createTreeView('viewcharset', {
-		treeDataProvider: new Provider(vscode.workspace.workspaceFolders[0].uri.path)
-	});
+	if (vscode.workspace.workspaceFolders !== undefined) {
+		vscode.window.createTreeView('viewcharset', {
+			treeDataProvider: new Provider(vscode.workspace.workspaceFolders[0].uri.path)
+		});
+	}
 }
 
 // this method is called when your extension is deactivated
