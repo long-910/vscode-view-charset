41b823ddaa0a2eceef012b108cc8f4c52323852d
long-kudo <910.long@gmail.com>
2021-02-23 20:56:37 +0900

    add file tree view

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,14 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { ViewCharsetProvider, Dependency } from './viewCharsetProvider';
+import { Provider} from './provider';
 
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-view-charset" is now active!');
+	console.log('extension "vscode-view-charset" is now active!');
 
 	vscode.window.createTreeView('viewcharset', {
-		treeDataProvider: new ViewCharsetProvider(vscode.workspace.rootPath)
+		treeDataProvider: new Provider(vscode.workspace.workspaceFolders[0].uri.path)
 	});
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-view-charset.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-view-charset!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
---
+++ src/provider.ts
@@ -0,0 +1,69 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export class Provider implements vscode.TreeDataProvider<TreeItem> {
+
+  constructor(private workspaceRoot: string) {
+  }
+
+  getTreeItem(element: TreeItem): vscode.TreeItem|Thenable<vscode.TreeItem> {
+    return element;
+  }
+
+  getChildren(element?: TreeItem|undefined): vscode.ProviderResult<TreeItem[]> {
+    if(!this.workspaceRoot){
+      return Promise.resolve([]);
+    }
+
+    if(element === undefined){
+      return Promise.resolve(this.getFiles(this.workspaceRoot));
+    }else{
+      return Promise.resolve(this.getFiles(path.join(element.parentPath, element.label)));
+    }
+  }
+
+  private getFiles(parentPath: string): TreeItem[] {
+    let treeItem: TreeItem[] = [];
+    if(this.pathExists(parentPath)){
+        let readDir = fs.readdirSync(parentPath);
+
+        //var encoding = require('encoding-japanese');
+        //console.log(encoding.detect(readDir));
+
+        readDir.forEach(fileName => {
+          let filePath:string = path.join(parentPath, fileName);
+          let fileColState:vscode.TreeItemCollapsibleState;
+          if(fs.statSync(filePath).isDirectory()){
+            fileColState = vscode.TreeItemCollapsibleState.Collapsed;
+          }else{
+            fileColState = vscode.TreeItemCollapsibleState.None;
+          }
+
+          treeItem.push(new TreeItem(fileName, parentPath, fileColState));
+        });
+    }
+    return treeItem;
+  }
+
+  private pathExists(p: string): boolean {
+    try {
+      fs.accessSync(p);
+    } catch (err) {
+      return false;
+    }
+    return true;
+  }
+}
+
+class TreeItem extends vscode.TreeItem {
+  children: TreeItem[]| undefined;
+
+  constructor(
+    public readonly label: string,
+    public readonly parentPath: string,
+    public readonly collapsibaleState: vscode.TreeItemCollapsibleState
+    ) {
+    super(label, collapsibaleState);
+  }
+}
