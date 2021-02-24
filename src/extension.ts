53b20c5a35c40f10e4028343b6b76cef6eebb650
long-kudo <910.long@gmail.com>
2021-02-25 04:50:56 +0900

    add coveralls

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,6 @@ import { Provider} from './provider';
 // this method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('extension "vscode-view-charset" is now active!');
-
 	if (vscode.workspace.workspaceFolders !== undefined) {
 		vscode.window.createTreeView('viewcharset', {
 			treeDataProvider: new Provider(vscode.workspace.workspaceFolders[0].uri.fsPath)
